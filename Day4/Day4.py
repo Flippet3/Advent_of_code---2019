63569fe4831d9def9f138c74644634196ab03eb4
Flippet3 <casperbekkers2@hotmail.com>
2019-12-04 10:59:50 +0100

    jepjep works now

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -25,20 +25,47 @@ class Digit:
                 return True;
         return False;
     
-    def CheckDouble(self, digits):
-        if self.value in digits:
+    def CheckDouble(self, next_dig):
+        if self.value == next_dig:
             return True;
         if self.pos != 'front':
-            digits = digits + [self.value];
-            return self.prev_digit.CheckDouble(digits);
+            return self.prev_digit.CheckDouble(self.value);
         return False;
-        
-    def nextIndexAllowed(self):
+    
+    def CheckDoubleNotTripple(self, next_dig):
+        if self.value == next_dig:
+            if (self.pos == 'front' or self.prev_digit.value != self.value):
+                return True;
+            dig = self;
+            while dig.prev_digit.value == dig.value:
+                if (dig.prev_digit.pos == 'front'):
+                    return False;
+                else:
+                    dig = dig.prev_digit;
+            return dig.prev_digit.CheckDoubleNotTripple(dig.value);
+            
+            
+        if self.pos != 'front':
+            return self.prev_digit.CheckDoubleNotTripple(self.value);
+        return False;
+    
+    def CheckAscending(self, next_dig):
+        if self.value <= next_dig:
+            if self.pos == 'front':
+                return True;
+            return self.prev_digit.CheckAscending(self.value);
+        return False;
+
+    def IndexAllowed(self, nottripple = False):
         if (self.atlimit()):
             return False;
         if (self.pos=='back'):
-            if (self.prev_digit.CheckDouble([self.value])):
-                return True;
+            if not (nottripple):
+                if (self.prev_digit.CheckDouble(self.value) and self.prev_digit.CheckAscending(self.value)):
+                    return True;
+            else:
+                if (self.prev_digit.CheckDoubleNotTripple(self.value) and self.prev_digit.CheckAscending(self.value)):
+                    return True;
         return False;
             
     def up(self):
@@ -48,15 +75,23 @@ class Digit:
 
 
 
-
+# ugly function for printing
+def printdigit(ldig):
+    print(str(last_digit.prev_digit.prev_digit.prev_digit.prev_digit.prev_digit.value)+
+          str(last_digit.prev_digit.prev_digit.prev_digit.prev_digit.value)+
+          str(last_digit.prev_digit.prev_digit.prev_digit.value)+
+          str(last_digit.prev_digit.prev_digit.value)+
+          str(last_digit.prev_digit.value)+
+          str(last_digit.value))
 
 
 #%%
 if (__name__ == '__main__'):
+    #Store in digits
     data = pd.read_csv('Input_Day4.txt', header=None).T;
     n1,n2 = data[0][0].split('-')
-    #n1 = '123456';
-    #n2 = '123800';
+    #n1 = '110800';
+    #n2 = '112100';
     for i in range(len(n1)):
         if i == 0:
             prev_digit = Digit(int(n1[i]), 'front', int(n2[i]))
@@ -65,33 +100,23 @@ if (__name__ == '__main__'):
         else:
             prev_digit = Digit(int(n1[i]), 'middle', int(n2[i]), prev_digit)
     
-    def printdigit(ldig):
-        print(str(last_digit.prev_digit.prev_digit.prev_digit.prev_digit.prev_digit.value)+
-              str(last_digit.prev_digit.prev_digit.prev_digit.prev_digit.value)+
-              str(last_digit.prev_digit.prev_digit.prev_digit.value)+
-              str(last_digit.prev_digit.prev_digit.value)+
-              str(last_digit.prev_digit.value)+
-              str(last_digit.value))
-    def nextdigit():
-        last_digit.up();
-        if (last_digit.nextIndexAllowed()):
-            return;
-        else:
-            if (last_digit.atlimit()):
-                return;
-            nextdigit();
+
         
-    possible_digits = 1;
+    possible_digits_1 = 0;
+    possible_digits_2 = 0;
     while not last_digit.atlimit():
-        nextdigit();
-        #printdigit(last_digit);
-        possible_digits += 1;
+        last_digit.up();
+        if (last_digit.IndexAllowed()):
+            possible_digits_1 += 1;
+        if (last_digit.IndexAllowed(True)):
+            possible_digits_2 += 1;
+            #printdigit(last_digit);
     
-    print("Day 3-1 answer is {0}".format(possible_digits));
+    print("Day 3-1 answer is {0}".format(possible_digits_1));
     
     
     
-    print("Day 3-2 answer is {0}".format(1));
+    print("Day 3-2 answer is {0}".format(possible_digits_2));
 
 
     
