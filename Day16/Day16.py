fdd8a2fbceb710813cf3fa449dba2d4b764ba0dc
Flippet3 <casperbekkers2@hotmail.com>
2019-12-16 10:48:09 +0100

    baseline 26 for first part solve

---
+++ Day16/Day16.py
@@ -0,0 +1,60 @@
+import time
+t0 = time.time();
+
+import  pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import parse
+import math
+#from Day9 import IntCode
+    
+
+            
+    
+    
+
+#%%
+if (__name__ == '__main__'):
+    #Load data.
+    plt.close('all')
+    #data = pd.read_csv('Input_Day16.txt', header=None);
+    data = pd.read_csv('Test.txt', header=None);
+    digit_str = data[0][0];
+    digit_len = len(digit_str);
+    digit = [int(i) for i in digit_str];
+    
+    # Create multiplication code.
+    multicode = np.zeros([digit_len, digit_len]);
+    multicode_base = [0,1,0,-1];
+    for i in range(digit_len):
+        layer_multicode = [item for item in multicode_base for j in range(i+1)];
+        layer_multicode *= digit_len//len(layer_multicode) + 1;
+        multicode[i,:] = layer_multicode[1:digit_len + 1];
+        # layer_i = mult
+    
+    iteration = 0;
+    while iteration < 100:
+        newdigit = [0] * digit_len;
+        for i in range(digit_len):
+            newdigit[i] = abs(sum([digit[j] * multicode[i,j] for j in range(digit_len)]))%10;
+        
+        
+        
+        digit = newdigit;
+        
+        iteration += 1;
+    
+    
+            
+    print("Day 16-1 answer is {0}".format(digit[0:8]));
+    t1= time.time();
+    
+    
+    print("Day 16-2 answer is {0}".format(1));
+
+
+    
+    #%%
+    t2 = time.time();
+    print ("Programme took {0} seconds to run".format(t1-t0));
+    print ("Programme took {0} seconds to run".format(t2-t1));
