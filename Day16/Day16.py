8fce2bff2fbafdaa3111792e296097fd377b2ef1
Flippet3 <casperbekkers2@hotmail.com>
2019-12-16 12:15:03 +0100

    found the trick

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -10,7 +10,8 @@ import math
     
 
             
-    
+def diglist2num (i_diglist) -> int:
+    return sum(i_diglist * 10**np.arange(len(i_diglist)-1, -1, -1));
     
 
 #%%
@@ -18,34 +19,44 @@ if (__name__ == '__main__'):
     #Load data.
     plt.close('all')
     data = pd.read_csv('Input_Day16.txt', header=None);
-    #data = pd.read_csv('Test.txt', header=None);
+    #data = pd.read_csv('Test2.txt', header=None);
     digit_str = data[0][0];
     digit_len = len(digit_str);
-    digit = np.array([int(i) for i in digit_str]).T;
+    digit = np.array([int(i) for i in digit_str], dtype='int32').T;
     
     # Create multiplication code.
-    multicode = np.zeros([digit_len, digit_len]);
-    multicode_base = [0,1,0,-1];
+    multicode = np.zeros([digit_len, digit_len], dtype='int32');
+    multicode_base = np.array([0,1,0,-1], dtype='int32');
     for i in range(digit_len):
-        layer_multicode = [item for item in multicode_base for j in range(i+1)];
-        layer_multicode *= digit_len//len(layer_multicode) + 1;
+        layer_multicode = np.repeat(multicode_base, i+1);
+        tile_nr = digit_len//len(layer_multicode) + 1;
+        if (tile_nr > 1):
+            layer_multicode = np.tile(layer_multicode, tile_nr);
         multicode[i,:] = layer_multicode[1:digit_len + 1];
-        # layer_i = mult
     
+    
+    phase = 100;
     iteration = 0;
-    while iteration < 100:
+    while iteration < phase:
         result_m = multicode * digit;
         digit = abs(result_m.sum(axis=1))%10
-        
         iteration += 1;
     
-    
             
-    print("Day 16-1 answer is {0}".format(digit[0:8]));
+    print("Day 16-1 answer is {0}".format(diglist2num(digit[0:8])));
     t1= time.time();
+    digit_str = data[0][0] * 10000;
+    digit = np.array([int(i) for i in digit_str], dtype='int32').T;
+    offset = diglist2num(digit[0:7]);
+    assert(offset > 0.5 * digit_len);
+    digit = digit[offset:];
     
+    iteration = 0
+    while iteration < phase:
+        digit = np.flip(np.cumsum(np.flip(digit)))%10;
+        iteration += 1;
     
-    print("Day 16-2 answer is {0}".format(1));
+    print("Day 16-2 answer is {0}".format(diglist2num(digit[0:8])));
 
 
     
