b10fcd612449006be91e7d4e649c04a41a85be86
Flippet3 <casperbekkers2@hotmail.com>
2019-12-18 21:00:49 +0100

    Day 17 and Day 18.1

--- Day17/Day17.py
+++ Day17/Day17.py
@@ -10,8 +10,18 @@ from Day9 import IntCode
 from random import random;
     
 
+def CheckScaffold(grid, pos):
+    if not (0 <= pos[0] < len(grid) and 0 <= pos[1] < len(grid[0])):
+        return False;
+    if not grid[pos[0]][pos[1]] == '#':
+        return False;
+    return True;
 
-    
+def printasciilist (my_list):
+    my_str = '';
+    for i in range(len(my_list)):
+        my_str += chr(my_list[i]);
+    print(my_str);
 
 #%%
 if (__name__ == '__main__'):
@@ -22,15 +32,159 @@ if (__name__ == '__main__'):
     intcode = IntCode(data.values.T[0]);
     intcode.execute(0);
     output = intcode.outputs;
-
-    print("Day 17-1 answer is {0}".format(1));
-    
+    grid = [];
+    gridline = '';
+    for i in range(len(output)):
+        if output[i] == 10:
+            if i != len(output)-1:
+                grid.append(gridline);
+                gridline = '';
+            continue;
+        gridline += chr(output[i]);
+    allignmentparams = []
+    for y in range(0,len(grid)):
+        for x in range(0,len(grid[0])):
+            if 1 < y < len(grid) - 1 and 1 < x < len(grid) - 1:
+                if grid[y][x] == '#':
+                    if grid[y-1][x] == '#' and\
+                        grid[y+1][x] == '#' and\
+                            grid[y][x+1] == '#' and\
+                                grid[y][x-1] == '#':
+                                    allignmentparams.append(y*x);
+            if grid[y][x] in '^><v':
+                move_dir = dict(zip('><v^', range(1,5)))[grid[y][x]];
+                pos = np.array([y,x]);
             
-    move_dirs = {1 : np.array([0,1]), 2 : np.array([0,-1]), 3 : np.array([-1,0]), 4 : np.array([1,0])}
-    t1= time.time();
-
+    print("Day 17-1 answer is {0}".format(sum(allignmentparams)));
     
-    print("Day 17-2 answer is {0}".format(1));
+            # East, West, South, North
+    move_dirs  = {1 : np.array([0,1]), 2 : np.array([0,-1]), 3 : np.array([1,0]), 4 : np.array([-1,0])}
+    right_dirs = {1 : 3, 2 : 4, 3 : 2, 4 : 1};
+    left_dirs  = {1 : 4, 2 : 3, 3 : 1, 4 : 2};
+    t1= time.time();
+    
+    
+    
+    # Create path
+    path = ''
+    
+    reached_end = False;
+    while not reached_end:
+        travelabledist = 0;
+        end = False;
+        while not end:
+            if not (CheckScaffold(grid, pos + move_dirs[move_dir])):
+                end = True;
+                continue;
+            else:
+                pos += move_dirs[move_dir];
+                travelabledist += 1;
+        if travelabledist != 0:
+            path += (str(travelabledist));
+        if CheckScaffold(grid, pos + move_dirs[right_dirs[move_dir]]):
+            path += ('R');
+            move_dir = right_dirs[move_dir];
+        elif CheckScaffold(grid, pos + move_dirs[left_dirs[move_dir]]):
+            path += ('L');
+            move_dir = left_dirs[move_dir];
+        else:
+            reached_end=True;
+            continue;
+    
+    #path = 'R8R8R4R4R8L6L2R4R4R8R8R8L6L2'
+    maxnr = 12;
+    
+    for i in range(2,maxnr):
+        A = path[0:i];
+        if (path[i] == '0'):
+            continue;
+        Apath = 'A'
+        ii = i;
+        j_low = 100;
+        while ii <= len(path) - i:
+            if path[ii:ii+i] == A:
+                Apath += 'A';
+                ii += i;
+            else:
+                j_low = min(len(Apath),j_low);
+                Apath += path[ii];
+                ii += 1;
+        Apath += path[ii:];
+        if j_low == 100:
+                j_low = 1;
+        # print('A: {0}'.format(A));
+        # print(Apath);
+        for j in range(2,maxnr):
+            B = Apath[j_low:j+j_low];
+            if ('A' in B) or (Apath[j_low+j] == '0'):
+                continue;
+            Bpath = Apath[0:j_low] + 'B'
+            jj = j_low + j;
+            k_low = 100;
+            while jj <= len(Apath) - j:
+                if Apath[jj:jj+j] == B:
+                    Bpath += 'B';
+                    jj += j;
+                else:
+                    if (Apath[jj] not in 'A'):
+                        k_low = min(len(Bpath),k_low);
+                    Bpath += Apath[jj];
+                    jj += 1;
+            if k_low == 100:
+                k_low = 1;
+            Bpath += Apath[jj:];
+            # print('B: {0}'.format(B));
+            # print(Bpath);
+            for k in range(2,maxnr):
+                C = Bpath[k_low:k_low+k];
+                if ('A' in C) or ('B' in C) or (Bpath[k_low+k] == '0'):
+                    continue;
+                Cpath = Bpath[0:k_low] + 'C'
+                kk = k_low + k;
+                succes = True;
+                while kk <= len(Bpath) - k:
+                    if Bpath[kk:kk+k] == C:
+                        Cpath += 'C';
+                        kk += k;
+                    else:
+                        Cpath += Bpath[kk];
+                        if Bpath[kk] not in 'AB':
+                            succes = False;
+                            #break;
+                        kk += 1;
+                Cpath += Bpath[kk:];
+                # print('C: {0}'.format(C));
+                # print(Cpath);
+                # if not 'L' in Cpath and not 'R' in Cpath:
+                #     print(Cpath);
+                #     print('wow');
+                if succes:
+                    print('======SUCCES=======\nA : {0}\nB : {1}\nC : {2}\n{3}'.format(A,B,C,Cpath));
+                    A_found = A;
+                    B_found = B;
+                    C_found = C;
+                    path_found = Cpath;
+                    break;
+        
+        
+    intcode.reset();
+    intcode.cur_memory[0] = 2;
+    
+    #intcode.execute(0)
+    for i in range(4):
+        my_str = [path_found,A_found,B_found,C_found][i];
+        for j in range(len(my_str)):
+            intcode.execute(ord(my_str[j]), True)
+            if j != len(my_str)-1 and my_str[j] != '1':
+                intcode.execute(ord(','), True);
+        intcode.execute(10, True);
+        print('========');
+        printasciilist(intcode.outputs);
+    intcode.execute(ord('n'), True);
+    intcode.execute(10, True);
+    printasciilist(intcode.outputs);
+    
+    print("Day 17-2 answer is {0}".format(intcode.outputs[-1]));
 
     
     #%%
--- Day18/Day18.py
+++ Day18/Day18.py
@@ -7,56 +7,148 @@ import matplotlib.pyplot as plt
 import parse
 import math
 #from Day9 import IntCode
+
+class Agent:
+    def __init__(self, pos, steps, keys):
+        self.pos = pos;
+        self.steps = steps;
+        self.keys = keys;
+    
+    def __repr__(self):
+        return 'Position: {0}\nSteps: {1}\nKeys: {2}'.format(self.pos, self.steps, self.keys);
+
+class Grid:
+    def __init__(self, values):
+        self.grid = []
+        self.Y = len(values);
+        self.X = len(values[0])
+        #UP, DOWN, LEFT, RIGHT;
+        self.dirs = {1 : [0,1], 2 : [0,-1], 3 : [-1,0], 4 : [1,0]};
+        for y in range(self.Y):
+            gridline = []
+            for x in range(self.X):
+                if values[y][x] == '@':
+                    self.pos = np.array([x,y]);
+                    gridline.append('.');
+                else:
+                    gridline.append(values[y][x]);
+            self.grid.append(gridline);
+    
+    def Get(self,*args):
+        if len(args) == 1:
+            args = args[0]
+        return self.grid[args[1]][args[0]];
+    
+    def Set(self, *args):
+        if len(args) == 2:
+            pos = args[0];
+            self.grid[pos[1], pos[0]] = args[1];
+        else:
+            self.grid[args[1]][args[0]] = args[2];
+    
+    def Reduce(self):
+        final = False;
+        while not final:
+            final = True;
+            for y in range(self.Y):
+                for x in range(self.X):
+                    if self.Get(x,y) == '.':
+                        around = self.GetAround(np.array([x,y]));
+                        if around.count('#') == 3:
+                            self.Set(x,y,'#');
+                            final = False;
+                            print('reduced')
+    
+    def FindKeys(self, pos, keys):
+        dirs = [1,2,3,4];
+        visited = set();
+        agents = {tuple(pos)}
+        steps = 0;
+        keys_found = set()
+        while len(agents) >= 1:
+            steps += 1;
+            newagents = set()
+            for agent in agents:
+                visited.add(agent);
+                around = self.GetAround(agent);
+                for i in range(len(around)):
+                    neighbor = around[i];
+                    if neighbor == '#':
+                        continue;
+                    ag_pos = (agent[0] + self.dirs[i+1][0], agent[1] + self.dirs[i+1][1]);
+                    if neighbor.isalpha():
+                        if neighbor.isupper():
+                            if not neighbor.lower() in keys:
+                                continue;
+                        else:
+                            if not neighbor in keys:
+                                keys_found.add((ag_pos, steps, neighbor));
+                                continue;
+                    if ag_pos not in visited:
+                        newagents.add(ag_pos);
+                #print ('inspected {0} from {1}'.format(agent, agents));
+            agents = newagents
+        return keys_found;
+        
+    def GetAround(self, pos):
+        pos = np.array(pos);
+        around = []
+        for i in range(4):
+            around.append(self.Get(pos + self.dirs[i+1]));
+        return around;
     
 
-            
-def diglist2num (i_diglist) -> int:
-    return sum(i_diglist * 10**np.arange(len(i_diglist)-1, -1, -1));
-    
 
 #%%
 if (__name__ == '__main__'):
     #Load data.
     plt.close('all')
-    data = pd.read_csv('Input_Day16.txt', header=None);
+    data = pd.read_csv('Input_Day18.txt', header=None);
     #data = pd.read_csv('Test2.txt', header=None);
-    digit_str = data[0][0];
-    digit_len = len(digit_str);
-    digit = np.array([int(i) for i in digit_str], dtype='int32').T;
+    data.columns = ['my_info']
+    grid = Grid(data.my_info.values)
+    grid.Reduce();
     
-    # Create multiplication code.
-    multicode = np.zeros([digit_len, digit_len], dtype='int32');
-    multicode_base = np.array([0,1,0,-1], dtype='int32');
-    for i in range(digit_len):
-        layer_multicode = np.repeat(multicode_base, i+1);
-        tile_nr = digit_len//len(layer_multicode) + 1;
-        if (tile_nr > 1):
-            layer_multicode = np.tile(layer_multicode, tile_nr);
-        multicode[i,:] = layer_multicode[1:digit_len + 1];
+    converged = False
+    # position, steps, keys;
+    agents = [Agent(grid.pos, 0, set())];
+    while not converged:
+        doubleagents = [];
+        newagents = [];
+        for i in range(len(agents)):
+            agent1 = agents[i];
+            for j in range(i+1,len(agents)):
+                agent2 = agents[j];
+                if np.all(agent1.pos == agent2.pos) and agent1.keys == agent2.keys:
+                    if agent1.steps > agent2.steps:
+                        if agent1 not in doubleagents:
+                            doubleagents.append(agent1);
+                    else:
+                        if agent2 not in doubleagents:
+                            doubleagents.append(agent2);
+        for i in range(len(doubleagents)):
+            print("double agent removed");
+            agents.remove(doubleagents[i]);
+        for agent in agents:
+            keys_grabable = grid.FindKeys(agent.pos, agent.keys);
+            for key_info in keys_grabable:
+                keys = agent.keys.copy();
+                keys.add(key_info[2]);
+                newagents.append(Agent(key_info[0], agent.steps + key_info[1], keys));
+        if len(newagents) == 0:
+            converged = True;
+            break;
+        else:
+            agents = newagents;
+        print(len(agents));
     
     
-    phase = 100;
-    iteration = 0;
-    while iteration < phase:
-        result_m = multicode * digit;
-        digit = abs(result_m.sum(axis=1))%10
-        iteration += 1;
-    
-            
-    print("Day 16-1 answer is {0}".format(diglist2num(digit[0:8])));
-    t1= time.time();
-    digit_str = data[0][0] * 10000;
-    digit = np.array([int(i) for i in digit_str], dtype='int32').T;
-    offset = diglist2num(digit[0:7]);
-    assert(offset > 0.5 * digit_len);
-    digit = digit[offset:];
-    
-    iteration = 0
-    while iteration < phase:
-        digit = np.flip(np.cumsum(np.flip(digit)))%10;
-        iteration += 1;
-    
-    print("Day 16-2 answer is {0}".format(diglist2num(digit[0:8])));
+    print("Day 18-2 answer is {0}".format(agents[0].steps));
+    t1 = time.time();
+
+
+
+    print("Day 18-1 answer is {0}".format(1));
 
 
     
